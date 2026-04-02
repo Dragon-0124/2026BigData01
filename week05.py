8c285e5e7ea671d8bc830750697b5eff4ebac3d8
Dragon-0124 <skseog0841@gmail.com>
2026-04-02 15:43:36 +0900

    week05 amusement park app v0.2

--- week05.py
+++ week05.py
@@ -1,3 +1,13 @@
-import ticket
+#import ticket
 
 humans = int(input("How many peoples do you with? "))
+
+ages=list()
+age = 0
+
+for i in range(humans):
+    age = int(input(f"What is the age of person {i + 1}? "))
+    ages.append(age)
+
+
+print(ages)
