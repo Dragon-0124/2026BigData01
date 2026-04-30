7db528eb7770acc9f0f51fd997bd07b58a56383a
Dragon-0124 <skseog0841@gmail.com>
2026-04-30 16:21:48 +0900

    week09 - Subset Observations : rows v0.1

--- week09.py
+++ week09.py
@@ -3,13 +3,10 @@ import pandas as pd
 df = pd.DataFrame( { '국' : [1, 6, 7], '영' : [2, 4, 8], '수' : [3, 5, 9], '화' : [10, 3, 11]}, index=[1, 2, 3] )
 print(df)
 
-# df_new = df.iloc[0:3] # df_new = df.iloc[:]
-# df_new = df.iloc[1:3] # df_new = df.iloc[1:]
-# df_new = df.iloc[1:, 2:]
 
-# df_new = df.iloc[:,[1, 3]]
-df_new = df.loc[df['국'] > 5, ['영','화']]
-print(df_new)
-
-print(df.iat[1, 2])
-print(df.at[2, '수'])
+print(df.sample(frac=0.33)) # Randomly select fraction of rows.
+print(df.sample(1)) # Randomly select n rows.
+print(df.nlargest(1,'국')) # Select and order top n entries.
+print(df.nsmallest(1,'국')) # Select and order bottom n entries.
+print(df.head(2)) # Select first n rows
+print(df.tail(2)) # Select last n rows
