c092ad401dc7a656b2205035ee438b124720ca59
Dragon-0124 <skseog0841@gmail.com>
2026-05-14 14:40:55 +0900

    week11 pandas v0.6 - nunique

---
+++ week11.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.DataFrame({'국':[7, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
+
+print(df)
+
+print("----------------------------------")
+
+print(df.nunique())
+
+print(len(df))
