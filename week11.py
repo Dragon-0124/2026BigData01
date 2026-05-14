9565167da403f64ad0f433faf5645df3baa47956
Dragon-0124 <skseog0841@gmail.com>
2026-05-14 16:22:26 +0900

    week11 pandas v0.9 - catplot

--- week11.py
+++ week11.py
@@ -9,3 +9,8 @@ ex = sns.load_dataset('exercise')
 # print(ex['time'].value_counts())
 # print(ex['diet'].value_counts())
 
+# print(len(ex[ex['diet'] == 'low fat']))
+# print(ex[ex['diet'] == 'low fat'])
+
+running_df = ex[ex['kind'] == 'running']
+sns.catplot(running_df, x = 'time', y = 'pulse', hue = 'diet', kind = 'point') # x축은 시간, y는 심박수, hue는 기준, kind는 그래프의 유형
