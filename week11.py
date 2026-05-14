763b369e27db1de41226d88a0ea7f9e10e6baa41
Dragon-0124 <skseog0841@gmail.com>
2026-05-14 16:34:00 +0900

    week11 pandas v1.0

--- week11.py
+++ week11.py
@@ -2,15 +2,13 @@ import seaborn as sns
 
 ex = sns.load_dataset('exercise')
 
-# print(ex.info())
-# print(ex.head(10))
+ex1 = ex[
+    (ex['diet'] == 'low fat')&
+    (ex['time'] == '30 min')&
+    (ex['kind'] == 'running')
+]
 
-# print(ex['kind'].value_counts())
-# print(ex['time'].value_counts())
-# print(ex['diet'].value_counts())
+print(ex1)
 
-# print(len(ex[ex['diet'] == 'low fat']))
-# print(ex[ex['diet'] == 'low fat'])
-
-running_df = ex[ex['kind'] == 'running']
-sns.catplot(running_df, x = 'time', y = 'pulse', hue = 'diet', kind = 'point') # x축은 시간, y는 심박수, hue는 기준, kind는 그래프의 유형
+mean_ex = ex1['pulse'].mean()
+print(mean_ex)
